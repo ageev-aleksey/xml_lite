8bd8861fa801ce4d74e9767cc84eb598f1b3efd2
nrx <ageevav97@gmail.com>
2020-07-12 14:42:48 +0300

    Добавлена поддержка systemd и был удален класс Directory, вместо него используется experimental/filesystem c++17

--- include/demon/DemonWorker.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by nrx on 11.07.2020.
-//
-
-#ifndef XML_DEMON_DEMONWORKER_H
-#define XML_DEMON_DEMONWORKER_H
-
-#endif //XML_DEMON_DEMONWORKER_H
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
 
 
 namespace fs = std::experimental::filesystem;
+constexpr int ERROR_EXIT = -1;
+constexpr int EXIT = 0;
 
 
 class App : public Demon::Worker {
@@ -45,32 +47,31 @@ public:
     }
     void start() override  {
         while(!isStop()) {
-            std::ifstream inputFile(mInputFileName);
-            if(inputFile.is_open()) {
-                std::stringstream sbuff;
-                sbuff << inputFile.rdbuf();
-                auto table = mParser.deserialize<Table>(sbuff.str());
-                inputFile.close();
-                char buff[5];
-                std::sprintf(buff,"%04d", ++mLastFileNumber);
-                fs::path tdp = mToDeletePath;
-                fs::rename(mInputFileName, tdp.append(mInputFileName.filename().string() + "." + buff));
-                std::ofstream fl(mFileNameFileNumber);
-                fl << mLastFileNumber;
-                fl.close();
-                std::ofstream outputFile(mOutputFile);
-                std::stringstream fbuff;
-                for(const auto &el : table.users) {
-                    fbuff << el.name << "\n"
-                          << el.date << "\n"
-                          << el.hoursWorked << "\n"
-                          << (el.carTrue ? "YES" : "NO") << "\n\n";
+                std::ifstream inputFile(mInputFileName);
+                if (inputFile.is_open()) {
+                    std::stringstream sbuff;
+                    sbuff << inputFile.rdbuf();
+                    auto table = mParser.deserialize<Table>(sbuff.str());
+                    inputFile.close();
+                    char buff[5];
+                    std::sprintf(buff, "%04d", ++mLastFileNumber);
+                    fs::path tdp = mToDeletePath;
+                    fs::rename(mInputFileName, tdp.append(mInputFileName.filename().string() + "." + buff));
+                    std::ofstream fl(mFileNameFileNumber);
+                    fl << mLastFileNumber;
+                    fl.close();
+                    std::ofstream outputFile(mOutputFile);
+                    std::stringstream fbuff;
+                    for (const auto &el : table.users) {
+                        fbuff << el.name << "\n"
+                              << el.date << "\n"
+                              << el.hoursWorked << "\n"
+                              << (el.carTrue ? "YES" : "NO") << "\n\n";
+                    }
+                    outputFile << fbuff.str();
+                    outputFile.close();
                 }
-                outputFile << fbuff.str();
-                outputFile.close();
-            }
-            std::this_thread::sleep_for(std::chrono::seconds(2));
-
+                std::this_thread::sleep_for(std::chrono::seconds(2));
         }
     }
 private:
@@ -85,7 +86,25 @@ private:
 #include <iostream>
 #include <string>
 
-int main() {
-    App app("input_data/test.xml", "output_data/out.txt", "to_delete/", "output_data/file_last_number.txt");
-    app.start();
+int main(int argc, char **argv) {
+    const std::string pathInputFile("input_data/test.xml");
+    const std::string pathOutputFile("output_data/out.txt");
+    const std::string pathToDelete("to_delete/");
+    const std::string pathLastNumberFile("output_data/file_last_number.txt");
+
+    if(argc > 1) {
+        if(strncmp(argv[1], "stop", 5) == 0) {
+            Demon::stop(argv[0]);
+            return EXIT;
+        }
+        std::cout << "error argument" << std::endl;
+        return ERROR_EXIT;
+    } else {
+        auto app  = std::make_shared<App>(pathInputFile, pathOutputFile, pathToDelete, pathLastNumberFile);
+        Demon d(app, argv[0]);
+        std::cout << "INIT" << std::endl;
+        d.start();
+    }
+
+    return EXIT;
 }
